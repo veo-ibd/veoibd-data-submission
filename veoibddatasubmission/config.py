829582bdba986fd8a4d517db5f57918afa02d1d4
kdaily <kenneth.daily@sagebase.org>
2019-09-17 13:49:24 -0700

    add config

--- veoibddatasubmission/config.py
+++ veoibddatasubmission/config.py
@@ -1,3 +1,12 @@
 """Configuration.
 
 """
+
+import genie.config
+
+from . import veoibd_clinical
+from . import veoibd_workflow
+
+PROCESS_FILES_LIST = [x for x in genie.config.get_subclasses(genie.config.BASE_CLASS)]
+
+PROCESS_FILES = genie.config.make_format_registry_dict(cls_list=PROCESS_FILES_LIST)
